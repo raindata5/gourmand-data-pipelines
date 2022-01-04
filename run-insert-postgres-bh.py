f6241ff0141a7f41687986f51c1414c49ccc2faa
Ronald <ronalddavisr3@gmail.com>
2022-01-03 19:19:02 -0500

    removed fetching of the result from the insert of data into postgres

--- run-insert-postgres-bh.py
+++ run-insert-postgres-bh.py
@@ -3,12 +3,14 @@ import psycopg2
 from postgres_bq_data_validation import db_conn
 
 ps_conn = db_conn()
+try:
 
-cursor = ps_conn.cursor()
-
-sql_file = open('insert-postgres-bh.sql','r')
-cursor.execute(sql_file.read())
-record = cursor.fetchone()
-res_1 = record[0]
-print(res_1)
-exit(0)
+    cursor = ps_conn.cursor()
+    sql_file = open('insert-postgres-bh.sql','r')
+    cursor.execute(sql_file.read())
+    # record = cursor.fetchone()
+    # res_1 = record[0]
+    # print(res_1)
+    exit(0)
+except Exception as e:
+    print(e)
